e23fa3acf6040e7c8909056706947d5a78d46a32
casout <calebsouthard01@gmail.com>
2018-04-24 12:39:07 -0500

    Add unit test

---
+++ testing/Vector3fTest.java
@@ -0,0 +1,58 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import math.Vector3f;
+
+public class Vector3fTest
+{
+  
+  @Test
+  public void testAdd() 
+  {        
+   Vector3f expected = new Vector3f(1,1,1);
+    Vector3f actual = new Vector3f (0.5f, 0.5f,0.5f).add(new Vector3f(0.5f, 0.5f,0.5f));
+    assertEquals(expected , actual); 
+
+  }
+
+  @Test
+  public void testSub() 
+  {
+    Vector3f expected = new Vector3f(0,0,0);
+    Vector3f actual = new Vector3f (0.9f, 0.9f,0.9f).sub(new Vector3f(0.9f, 0.9f,0.9f));
+    assertEquals(expected , actual); 
+    
+
+  }
+
+  @Test
+  public void testMul() 
+  {
+    Vector3f expected = new Vector3f(6.25f,6.25f,6.25f);
+    Vector3f actual = new Vector3f (2.5f, 2.5f,2.5f).mul(new Vector3f(2.5f, 2.5f,2.5f));
+    assertEquals(expected , actual); 
+    
+
+  }
+  
+  
+  @Test
+  public void testClone() 
+  {
+	  Vector3f expected = new Vector3f(6.25f,6.25f,6.25f);
+	  Vector3f actual = expected.clone();
+	  assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void testToString() 
+  {
+	  Vector3f expected = new Vector3f(6.25f,6.25f,6.25f);
+	  String actual = "(6.25, 6.25, 6.25)";
+	  assertEquals(expected.toString(),actual);
+  }
+
+}
